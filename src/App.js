ee1fe955bb0dad407c7a90e8db98006a024b683b
Rafael Ramos <rbr.ramos@gmail.com>
2020-08-23 20:20:18 +0100

    modified 'App.js' file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Updated by the pull request
+          Updated by the pull request! :D
         </p>
         <a
           className="App-link"
