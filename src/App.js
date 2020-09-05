17da590281d3cd599d3cd837037debfb77d4a199
Rafael Ramos <rbr.ramos@gmail.com>
2020-09-05 16:02:36 +0100

    ajuste de identação no Dockerfile

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Updated by Pull Request! :D
+          Updated by Pull Request
         </p>
         <a
           className="App-link"
