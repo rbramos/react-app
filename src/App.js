c208ac661655b817d062b8eb6cae6159ac9890da
Rafael Ramos <rbr.ramos@gmail.com>
2020-09-05 16:47:39 +0100

    teste de alteração

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Updated by Pull Request
+          Aula TGS 05/09
         </p>
         <a
           className="App-link"
